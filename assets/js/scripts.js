462589c176216e947c3dbd1cf37799cf353221c1
Stephen Fike <sfike98@gmail.com>
2022-03-21 20:26:05 -0500

    adding functionality to change city name

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,7 @@
 var apiKey = "6f72019040ff1a5c35e180b603fe5518";
 var cityNameEl = $("#city");
+var currentWeatherContainerEl = $(".currentWeather");
+var fiveDayForecastContainerEl = $(".fiveDayForecast");
 
 var formSubmitHandler = function (event) {
   event.preventDefault();
@@ -30,6 +32,17 @@ var getHistory = function() {
     console.log(cityArray);
 };
 
+var displayCityWeather = function(weather) {
+  // create h1 for the current date
+
+  // create h2 for city name
+  var nameContainer = document.createElement("h2");
+  nameContainer.textContent = weather.name
+  // append to currentWeather container
+  currentWeatherContainerEl.append(nameContainer);
+
+}
+
 var getCityWeather = function (city) {
   var apiUrl =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
@@ -39,6 +52,7 @@ var getCityWeather = function (city) {
   fetch(apiUrl).then(function (response) {
     if (response.ok) {
       response.json().then(function (data) {
+        displayCityWeather(data);
         getLocalStorage(data);
         console.log(data);
       });
